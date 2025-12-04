6f1d081f856eec102165da4a6bcf3cb315cc5d76
Alejandro Huesca <91446260+PeinSoR@users.noreply.github.com>
2025-12-04 13:12:29 -0600

    Endpoint usuarios colonias

---
+++ src/main/java/com/projectframe/mx/petcare/dominio/infraestructura/usuariosColoniasControlador.java
@@ -0,0 +1,51 @@
+package com.projectframe.mx.petcare.dominio.infraestructura;
+
+import com.projectframe.mx.petcare.dominio.aplicacion.usuariosColoniasServicio;
+import com.projectframe.mx.petcare.dominio.entidades.usuariosColonias;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/petcare")
+public class usuariosColoniasControlador {
+    @Autowired
+    private usuariosColoniasServicio  usuariosColoniasServicio;
+
+    @GetMapping("/allusuarios-colonias")
+    @ResponseStatus(HttpStatus.OK)
+    public List<usuariosColonias> obtenerAllUsuariosColonias(){
+        return usuariosColoniasServicio.obtenerUsuariosColonias();
+    }
+
+    @GetMapping("/usuarios-colonias/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public usuariosColonias obtenerUsuariosColonias(@PathVariable Long id){
+        return usuariosColoniasServicio.obtenerUsuariosColoniasPorId(id);
+    }
+
+    @PostMapping("/create-usuarios-colonias")
+    @ResponseStatus(HttpStatus.OK)
+    public usuariosColonias guardarUsuariosColonias(@RequestBody usuariosColonias usuariosColonias){
+        return usuariosColoniasServicio.guardarUsuariosColonias(usuariosColonias);
+    }
+
+    @PutMapping("/update-usuarios-colonias")
+    @ResponseStatus(HttpStatus.OK)
+    public usuariosColonias actualizarUsuarioColonia(@RequestBody usuariosColonias usuariosColonias, @PathVariable Long id){
+        usuariosColonias uc =  usuariosColoniasServicio.obtenerUsuariosColoniasPorId(id);
+        uc.setColoniaId(usuariosColonias.getColoniaId());
+        uc.setUsuarioId(usuariosColonias.getUsuarioId());
+        uc.setFechaRegistro(usuariosColonias.getFechaRegistro());
+        return usuariosColoniasServicio.guardarUsuariosColonias(uc);
+    }
+
+    @DeleteMapping("/delete-usuarios-colonias/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void eliminarUsuarioColonia(@PathVariable Long id){
+        usuariosColoniasServicio.eliminarUsuariosColonias(id);
+    }
+}
